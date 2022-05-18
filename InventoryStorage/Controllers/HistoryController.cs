aed328dde84a35d6d16c2a3ddde6fdad912265b1
Ангел Коларов <angelkolarov17a@gmail.com>
2022-05-18 22:02:57 +0300

    Add project files.

---
+++ InventoryStorage/Controllers/HistoryController.cs
@@ -0,0 +1,42 @@
+﻿using InventoryStorage.Data;
+using InventoryStorage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace InventoryStorage.Controllers
+{
+    public class HistoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        public HistoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<ItemHistory> obj = _db.Histories;
+            return View(obj);
+        }
+        public IActionResult Search(string searchBy, string search)
+        {
+            if (searchBy == "Id")
+            {
+                return View(_db.Histories.Where(x => x.Id.ToString().StartsWith(search)).ToList());
+            }
+            //else if (searchBy == "Description")
+            //{
+            //    return View(_db.Histories.Where(x => x.Description.ToLower().StartsWith(search)).ToList());
+            //}
+            //else if (searchBy == "ItemName")
+            //{
+            //    return View(_db.Histories.Where(x => x.ItemName.ToLower().StartsWith(search)).ToList());
+            //}
+            return View(_db.Histories.Where(x => x.Location.ToString().StartsWith(search)).ToList());
+        }
+    }
+}
---
+++ InventoryStorage/Controllers/SearchController.cs
@@ -0,0 +1,77 @@
+﻿using InventoryStorage.Data;
+using InventoryStorage.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace InventoryStorage.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public SearchController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> obj = _db.Items;
+            return View(obj);
+        }
+       
+        public IActionResult Search(string searchBy, string search)
+        {
+            if (searchBy == "Id")
+            {
+                return View(_db.Items.Where(x => x.Id.ToString().StartsWith(search)).ToList());
+            }
+            else if (searchBy == "Description")
+            {
+                return View(_db.Items.Where(x => x.Description.ToLower().StartsWith(search)).ToList());
+            }
+            else if (searchBy == "ItemName")
+            {
+                return View(_db.Items.Where(x => x.ItemName.ToLower().StartsWith(search)).ToList());
+            }
+            return View(_db.Items.Where(x => x.Location.ToString().StartsWith(search)).ToList());
+        }
+        public IActionResult Update(int? Id)
+        {
+            var obj = _db.Items.Find(Id);
+
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //Update POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Item obj)
+        {
+            if (obj == null)
+            {
+                return NotFound();
+            }
+                _db.Items.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+        }
+        public IActionResult Delete(int? Id)
+        {
+            var obj = _db.Items.Find(Id);
+
+            if (Id == null)
+            {
+                return NotFound();
+            }
+            _db.Items.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
